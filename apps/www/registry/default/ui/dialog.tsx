c53872d0e8711209e05d143ee1446160e5839908
felipetodev <fe.ossandon.u@gmail.com>
2024-08-17 00:00:56 -0400

    fix(www): dialog color styles

--- apps/www/registry/default/ui/dialog.tsx
+++ apps/www/registry/default/ui/dialog.tsx
@@ -133,6 +133,7 @@ const StyledDialogClose = styled(DialogPrimitive.Close)`
   transition-property: opacity;
   transition-timing-function: cubic-bezier(.4,0,.2,1);
   transition-duration: .15s;
+  color: inherit;
 
   &:hover {
     opacity: 1;
@@ -155,7 +156,7 @@ const DialogContent = React.forwardRef<
     >
       {children}
       <StyledDialogClose>
-        <svg aria-label="close-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <svg aria-label="close-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
           <path d="M18 6 6 18" />
           <path d="m6 6 12 12" />
         </svg>
